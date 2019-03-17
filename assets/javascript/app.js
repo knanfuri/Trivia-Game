4756e1b878f339247a80642b87697271b796c2c8
knanfuri <knanfuri@yahoo.com>
2019-03-17 11:30:18 -0500

    Updated JS, Css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,132 +1,180 @@
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
-}
-Question.prototype.correctAnswer = function(choice) {
-  return choice === this.answer;
-};
+// select all elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
 
-function Quiz(question) {
-  this.score = 0;
-
-  this.questions = questions;
-
-  this.questionIndex = 0;
-}
-
-Quiz.prototype.getQuestionIndex = function() {
-  return this.question[this.questionIndex];
-};
-
-Quiz.prototype.isEnded = function() {
-  return this.questions.length === this.questionIndex;
-};
-
-Quiz.prototype.guess = function(answer) {
-  this.questionIndex++;
-  if (this.getQuestionIndex().correctAnswer(answer)) {
-    this.score++;
-  }
-};
-
-function populate() {
-  if (quiz.isEnded()) {
-    ShowScores();
-  } else {
-    var element = document.getElementById("question");
-    element.innerHTML = quiz.getQuestionIndex().text;
-
-    var choices = quiz.getQuestionIndex().choices;
-    for (var i = 0; i < choices.length; i++) {
-      var element = document.getElementById("choice" + i);
-      element.innerHTML = choices[i];
-      guess("btn" + i, choices[i]);
-    }
-
-    showprogress();
-  }
-}
-
-function guess(id, guess) {
-  var button = document.getElementById(id);
-  button.onclick = function() {
-    quiz.guess(guess);
-    populate();
-  };
-}
-
-function showprogress() {
-  var currentQuestionNumber = quiz.questionIndex + 1;
-  var element = document.getElementById("progress");
-  element.innerHTML =
-    "Questions " + currentQuestionNumber + "of" + quiz.question.length;
-}
-
-function showScores() {
-  var gameOverHtml = "<h1>Result</h1>";
-  gameOverHtml += "<h2 id = 'score'> Your score: " + quiz.score + "</h2>";
-  var element = document.getElementById("quiz");
-  element.innerHTML = gameOverHtml;
-}
-
+// create our questions
 var questions = [
-  new Question(
-    "What state is chicago in?",
-    ["Washington", "Ohio", "Illinois"],
-    "illinois"
-  ),
-  new Question(
-    "What continent is Australia located in?",
-    ["Australia", "Ohio", "Illinois"],
-    "Australia"
-  ),
-  new Question(
-    "What continent is New Zealand located in?",
-    ["Washington", "Ohio", "Illinois"],
-    "Australia"
-  ),
-  new Question("Is Java the same as Javascript", ["Yes", "No"], "No"),
-  new Question(
-    "China has the largest population compared to india",
-    ["True", "false"],
-    "True"
-  )
+  {
+    question: "What continent is Australia located in?",
+
+    choiceA: "New Zealand",
+    choiceB: "Australia",
+    choiceC: "Europe",
+    correct: "B"
+  },
+  {
+    question: "What continent is NewZealand located in?",
+
+    choiceA: "New Zealand",
+    choiceB: "Australia",
+    choiceC: "Europe",
+    correct: "B"
+  },
+  {
+    question: "Who is the 43rd president of USA?",
+
+    choiceA: "Donald Trump",
+    choiceB: "Bill Clinton",
+    choiceC: "George Bush",
+    correct: "C"
+  },
+
+  {
+    question: "Who is the 43rd president of USA?",
+
+    choiceA: "Donald Trump",
+    choiceB: "Bill Clinton",
+    choiceC: "George Bush",
+    correct: "C"
+  },
+
+  {
+    question: "Who is the 43rd president of USA?",
+
+    choiceA: "Donald Trump",
+    choiceB: "Bill Clinton",
+    choiceC: "George Bush",
+    correct: "C"
+  }
 ];
 
-var quiz = new quiz(questions);
+// create some variables
 
-populate();
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
 
-// function Question(text, choices, answer) {
-//   this.text = text;
-//   this.choices = choices;
-//   this.answer = answer;
-// }
+// render a question
+function renderQuestion() {
+  let q = questions[runningQuestion];
 
-// Question.prototype.correctAnswer = function(choice) {
-//   return choice === this.answer;
-// };
+  question.innerHTML = "<p>" + q.question + "</p>";
+  qImg.innerHTML = "<img src=" + q.imgSrc + ">";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choiceD.innerHTML = q.choiceD;
+  choiceE.innerHTML = q.choiceE;
+}
 
-// function Quiz(question) {
-//   this.score = 0;
+start.addEventListener("click", startQuiz);
 
-//   this.questions = questions;
+// start quiz
+function startQuiz() {
+  start.style.display = "none";
+  renderQuestion();
+  quiz.style.display = "block";
+  renderProgress();
+  renderCounter();
+  TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
+}
 
-//   this.questionIndex = 0;
-// }
+// render progress
+function renderProgress() {
+  for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+  }
+}
 
-// Quiz.prototype.getQuestionIndex = function() {
-//   return this.question[this.questionIndex];
-// };
+// counter render
 
-// Quiz.prototype.isEnded = function() {
-//   return this.questions.length === this.questionIndex;
-// };
+function renderCounter() {
+  if (count <= questionTime) {
+    counter.innerHTML = count;
+    timeGauge.style.width = count * gaugeUnit + "px";
+    count++;
+  } else {
+    count = 0;
+    // change progress color to red
+    answerIsWrong();
+    if (runningQuestion < lastQuestion) {
+      runningQuestion++;
+      renderQuestion();
+    } else {
+      // end the quiz and show the score
+      clearInterval(TIMER);
+      scoreRender();
+    }
+  }
+}
 
-// Quiz.prototype.guess = function(answer) {
-//   this.questionIndex++;
-//   if (this.getQuestionIndex().correctAnswer(answer)) {
-//     this.score++;
-//   }
-// };
+// checkAnwer
+
+function checkAnswer(answer) {
+  if (answer == questions[runningQuestion].correct) {
+    // answer is correct
+    score++;
+    // change progress color to green
+    answerIsCorrect();
+  } else {
+    // answer is wrong
+    // change progress color to red
+    answerIsWrong();
+  }
+  count = 0;
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+  } else {
+    // end the quiz and show the score
+    clearInterval(TIMER);
+    scoreRender();
+  }
+}
+
+// answer is correct
+function answerIsCorrect() {
+  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+// answer is Wrong
+function answerIsWrong() {
+  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+// score render
+function scoreRender() {
+  scoreDiv.style.display = "block";
+
+  // calculate the amount of question percent answered by the user
+  const scorePerCent = Math.round((100 * score) / questions.length);
+
+  // choose the image based on the scorePerCent
+  let img =
+    scorePerCent >= 80
+      ? "img/5.png"
+      : scorePerCent >= 60
+      ? "img/4.png"
+      : scorePerCent >= 40
+      ? "img/3.png"
+      : scorePerCent >= 20
+      ? "img/2.png"
+      : "img/1.png";
+
+  scoreDiv.innerHTML = "<img src=" + img + ">";
+  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+}
